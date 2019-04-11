ff7e91e19ad148175e269301a5a002012a830efb
AuyeskhanM <auyeskhanm@gmail.com>
2019-04-11 09:07:12 +0600

    Добавьте файлы проекта.

---
+++ dz04.04/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dz04._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StarWarsSwapiCo starWarsSwapiCo = new StarWarsSwapiCo();
+            starWarsSwapiCo.StarWars();
+            Console.ReadLine();
+        }
+    }
+}
